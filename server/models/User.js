8a14d4595ca9dbae610f9bbd6e3f6d281cc3f6ca
Karl Divinagracia <mtd28123@gmail.com>
2026-03-21 15:06:05 +0800

    removed bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -51,16 +50,9 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-// Hash password before saving
-userSchema.pre('save', async function () {
-    if (!this.isModified('password')) return;
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
-
 // Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+    return candidatePassword === this.password;
 };
 
 // Remove password when converting to JSON
