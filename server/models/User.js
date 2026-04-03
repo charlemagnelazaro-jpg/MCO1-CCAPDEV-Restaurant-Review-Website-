eb6c284309363da4bd820099e3ee1bbf4e1bcf42
Karl Divinagracia <161958514+lraaak@users.noreply.github.com>
2026-04-03 21:14:58 +0800

    re-added bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -50,9 +51,16 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
+// Hash password before saving
+userSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 // Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return candidatePassword === this.password;
+    return await bcrypt.compare(candidatePassword, this.password);
 };
 
 // Remove password when converting to JSON
