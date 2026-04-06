f49527ce26e18716e66e435762d12f92c4bf90d7
charlemagnelazaro-jpg <charlemagne_lazaro@dlsu.edu.ph>
2026-04-06 15:47:27 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(cors({
 app.set('trust proxy', 1);
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "ArchersSecretKey"
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
